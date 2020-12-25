8cc3b79ae12c1a72e0582d22ae7451ff2dd59906
Bartlomiej Kregielewski <bartlomiejkregielewski@gmail.com>
2020-12-25 01:35:23 +0100

    Initial config and wolves entity added

--- Rabbit-Island/Config.cs
+++ Rabbit-Island/Config.cs
@@ -6,9 +6,57 @@ namespace Rabbit_Island
 {
     internal class Config
     {
+        public interface ICreatureConfig<Creature> where Creature : Entities.Creature
+        {
+            public Type CreatureType { get; set; }
+
+            public int InitialPopulation { get; set; }
+
+            public int MinChildren { get; set; }
+
+            public int MaxChildren { get; set; }
+        }
+
+        private class CreatureConfig<Creature> : ICreatureConfig<Creature> where Creature : Entities.Creature
+        {
+            public CreatureConfig()
+            {
+                CreatureType = typeof(Creature);
+            }
+
+            public Type CreatureType { get; set; }
+
+            public int InitialPopulation { get; set; }
+
+            public int MinChildren { get; set; }
+
+            public int MaxChildren { get; set; }
+        }
+
         public Config()
         {
-            throw new NotImplementedException();
+            RabbitConfig = new CreatureConfig<Entities.Rabbit>();
+            WolvesConfig = new CreatureConfig<Entities.Wolf>();
         }
+
+        public double TimeRate { get; set; }
+
+        public bool DeathFromOldAge { get; set; }
+
+        public int MaxCreatures { get; set; }
+
+        public int FruitsPerDay { get; set; }
+
+        public int PregnacyDurationDays { get; set; }
+
+        public bool FoodExpires { get; set; }
+
+        public bool DrawRanges { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public ICreatureConfig<Entities.Rabbit> RabbitConfig { get; }
+
+        public ICreatureConfig<Entities.Wolf> WolvesConfig { get; }
     }
 }
---
+++ Rabbit-Island/Entities/Wolf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit_Island.Entities
+{
+    internal class Wolf : Creature
+    {
+        public Wolf(float x, float y) : base(x, y)
+        {
+        }
+
+        public override void Act()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
